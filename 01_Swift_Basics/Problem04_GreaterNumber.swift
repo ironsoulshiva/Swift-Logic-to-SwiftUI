29a6b4b4f1cef9512d3266efcbd3f67d25a6f889
Mattapathi Kumar <Mattaparthi.Kumar@HAP-483s-MacBook-Air.local>
2025-12-28 21:56:39 +0530

    Code update

--- 01_Swift_Basics/Problem04_GreaterNumber.swift
+++ 01_Swift_Basics/Problem04_GreaterNumber.swift
@@ -1,13 +1,12 @@
 // 4. Compare two numbers and find the greater
 
-print("Enter the number")
+print("Enter the number1")
+print("Enter the number2")
 
-if let num1 = Int(readLine() ?? "") , let num2 = Int(readLine() ?? "") {
+if let firstInput = readLine(), let num1 = Int(firstInput) , let secondInput = readLine() , let num2 = Int(secondInput) {
     if num1 > num2 {
-        print("Num1 is greater")
+        print("Number1 is greater")
     } else {
-        print("Num2 is greater")
+        print("Number2 is greater")
     }
-} else {
-    print("Invalid Number")
 }
